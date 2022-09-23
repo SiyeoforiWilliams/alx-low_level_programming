c64d0f9ff0580bcdb0dba194f6708d2650d1bd39
SiyeoforiWilliams <siyeoforiwilliams@gmail.com>
2022-09-23 04:09:24 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,9 +5,9 @@
 *
 */
 void print_number(int n)
-{
+{	
 	unsigned int num = n;
-
+	
 	if (n < 0)
 	{
 		_putchar('-');
@@ -17,5 +17,5 @@ void print_number(int n)
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	putchar(((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
